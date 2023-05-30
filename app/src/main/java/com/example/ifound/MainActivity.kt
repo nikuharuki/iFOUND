42811eaf7758cd22b2efbf0bbb483267052d215c
Nikos Pasion <nikosladerapasion@gmail.com>
2023-05-30 13:59:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ifound/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.ifound
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ifound.databinding.ActivityMainBinding
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNavView.menu.getItem(1).isEnabled = false
+
+
+        //Corner radius
+//        val radius = resources.getDimension(R.dimen.default_corner_radius)
+//        val bottomAppBar = findViewById<BottomAppBar>(R.id.bottom_app_bar)
+//
+//        val bottomBarBackground = bottomAppBar.background as MaterialShapeDrawable
+//        bottomBarBackground.shapeAppearanceModel = bottomBarBackground.shapeAppearanceModel
+//            .toBuilder()
+//            .setTopRightCorner(CornerFamily.ROUNDED, radius)
+//            .setTopLeftCorner(CornerFamily.ROUNDED, radius)
+//            .build()
+
+
+    }
+}
