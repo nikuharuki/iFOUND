eefb1fd3fffeee8d7116494ac7de755d39fd67f0
Yves Domingo <yvesrafaeladomingo@gmail.com>
2023-06-26 14:38:57 +0800

    - started and finished activity_found_item_page.xml

---
+++ app/src/main/java/com/example/ifound/FoundItemPageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ifound
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// FoundItemPage =/= FoundItemFeed
+
+class FoundItemPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_found_item_page)
+    }
+}
