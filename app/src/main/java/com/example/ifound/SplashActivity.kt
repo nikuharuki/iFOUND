0bbe1b2ba197461426837ba8476e70e89a5d2767
Yves Domingo <yvesrafaeladomingo@gmail.com>
2023-07-02 04:11:40 +0800

    - added persistent authentication for firebase log ig

--- app/src/main/java/com/example/ifound/SplashActivity.kt
+++ app/src/main/java/com/example/ifound/SplashActivity.kt
@@ -3,17 +3,38 @@ package com.example.ifound
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import java.util.Timer
 import kotlin.concurrent.schedule
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         Timer().schedule(3000) {
-            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
-            finish()
+            val currentUser = firebaseAuth.currentUser
+
+            if (currentUser != null) {
+                navigateToMainScreen()
+            } else {
+                navigateToLoginScreen()
+            }
         }
+    }
+
+    private fun navigateToLoginScreen() {
+        startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+        finish()
+    }
+
+    private fun navigateToMainScreen() {
+        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        finish()
     }
 }
