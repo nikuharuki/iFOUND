6865f22bd05286deaab7cf2a25532a5b3c5ccf2a
Nikos Pasion <nikosladerapasion@gmail.com>
2023-07-11 23:31:52 +0800

    di pa to ticket system. i just fixed yung conflict kasi nagbago layout from et to radio buttons

--- app/src/main/java/com/example/ifound/FoundItemFormActivity.kt
+++ app/src/main/java/com/example/ifound/FoundItemFormActivity.kt
@@ -23,93 +23,94 @@ import com.google.firebase.database.FirebaseDatabase
 import java.io.ByteArrayOutputStream
 
 class FoundItemFormActivity : AppCompatActivity() {
-    var sImage : String = ""
-    private lateinit var binding : ActivityFoundItemFormBinding
-    private lateinit var databaseReference:  DatabaseReference
+    var sImage: String = ""
+    private lateinit var binding: ActivityFoundItemFormBinding
+    private lateinit var databaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityFoundItemFormBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val cameraIntentLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(), ActivityResultCallback {
-            if(it.resultCode == RESULT_OK){
-                val photo : Bitmap = it.data?.extras?.get("data") as Bitmap
-                binding.ivPhoto.setImageBitmap(photo)
-            }
-        })
-
-        val galleryIntentLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(), ActivityResultCallback {
-            if(it.resultCode == Activity.RESULT_OK){
-                val imageUri = it.data?.data
-                val imageStream = imageUri?.let { it1 -> contentResolver.openInputStream(it1) }
-                val photo = BitmapFactory.decodeStream(imageStream)
-                binding.ivPhoto.setImageBitmap(photo)
-            }
-        })
-
-        binding.btnAddPhoto.setOnClickListener {
-                val permissions = arrayOf(Manifest.permission.CAMERA)
-                if (ContextCompat.checkSelfPermission(this@FoundItemFormActivity, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(this@FoundItemFormActivity, permissions, 1)
-                }else{
-//                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//                cameraIntentLauncher.launch(intent)
-
-                    val intent = Intent(Intent.ACTION_GET_CONTENT)
-                    intent.type = "image/*"
-                    ActivityResultLauncher.launch(intent)
-                }
-        }
-
-        binding.btnSubmit.setOnClickListener {
-            val item = binding.etWhatHaveYouFoundFoundItemForm.text.toString()
-            val addInfo = binding.etAnyAdditionalInfoFoundItemForm.toString()
-            val whereFound = binding.etILostMyLostForm.toString()
-            val whatRoom = binding.etRoomFoundItemForm.toString()
-            val name = binding.etNameFoundItemForm.toString()
-            val phoneNo = binding.etPhoneFoundItemForm.text.toString()
-            val email = binding.etEmailFoundItemForm.toString()
-
-            databaseReference = FirebaseDatabase.getInstance().getReference("Found Items")
-            val foundItemData = FoundItemData(item, addInfo, whereFound, whatRoom, name, phoneNo, email, sImage)
-            databaseReference.child(item).setValue(foundItemData).addOnSuccessListener {
-                binding.etWhatHaveYouFoundFoundItemForm.text.clear()
-                binding.etAnyAdditionalInfoFoundItemForm.text.clear()
-                binding.etILostMyLostForm.text.clear()
-                binding.etRoomFoundItemForm.text.clear()
-                binding.etNameFoundItemForm.text.clear()
-                binding.etPhoneFoundItemForm.text.clear()
-                binding.etEmailFoundItemForm.text.clear()
-                sImage = ""
-
-                Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
-
-                val intent = Intent(this@FoundItemFormActivity,MainActivity::class.java )
-                startActivity(intent)
-                finish()
-            }.addOnFailureListener{
-                Toast.makeText(this,"Failed", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-    private val ActivityResultLauncher = registerForActivityResult<Intent, ActivityResult>(
-        ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-        if (result.resultCode == RESULT_OK) {
-            val uri = result.data!!.data
-            try {
-                val inputStream = contentResolver.openInputStream(uri!!)
-                val bitmap = BitmapFactory.decodeStream(inputStream)
-                val stream = ByteArrayOutputStream()
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-                val bytes = stream.toByteArray()
-                sImage = Base64.encodeToString(bytes, Base64.DEFAULT)
-                binding.ivPhoto.setImageBitmap(bitmap)
-                inputStream!!.close()
-
-            } catch (e: Exception) {
-                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
-            }
-        }
+//        val cameraIntentLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(), ActivityResultCallback {
+//            if(it.resultCode == RESULT_OK){
+//                val photo : Bitmap = it.data?.extras?.get("data") as Bitmap
+//                binding.ivPhoto.setImageBitmap(photo)
+//            }
+//        })
+//
+//        val galleryIntentLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(), ActivityResultCallback {
+//            if(it.resultCode == Activity.RESULT_OK){
+//                val imageUri = it.data?.data
+//                val imageStream = imageUri?.let { it1 -> contentResolver.openInputStream(it1) }
+//                val photo = BitmapFactory.decodeStream(imageStream)
+//                binding.ivPhoto.setImageBitmap(photo)
+//            }
+//        })
+//
+//        binding.btnAddPhoto.setOnClickListener {
+//                val permissions = arrayOf(Manifest.permission.CAMERA)
+//                if (ContextCompat.checkSelfPermission(this@FoundItemFormActivity, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//                    ActivityCompat.requestPermissions(this@FoundItemFormActivity, permissions, 1)
+//                }else{
+////                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+////                cameraIntentLauncher.launch(intent)
+//
+//                    val intent = Intent(Intent.ACTION_GET_CONTENT)
+//                    intent.type = "image/*"
+//                    ActivityResultLauncher.launch(intent)
+//                }
+//        }
+//
+//        binding.btnSubmit.setOnClickListener {
+//            val item = binding.etWhatHaveYouFoundFoundItemForm.text.toString()
+//            val addInfo = binding.etAnyAdditionalInfoFoundItemForm.toString()
+//            val whereFound = binding.etILostMyLostForm.toString()
+//            val whatRoom = binding.etRoomFoundItemForm.toString()
+//            val name = binding.etNameFoundItemForm.toString()
+//            val phoneNo = binding.etPhoneFoundItemForm.text.toString()
+//            val email = binding.etEmailFoundItemForm.toString()
+//
+//            databaseReference = FirebaseDatabase.getInstance().getReference("Found Items")
+//            val foundItemData = FoundItemData(item, addInfo, whereFound, whatRoom, name, phoneNo, email, sImage)
+//            databaseReference.child(item).setValue(foundItemData).addOnSuccessListener {
+//                binding.etWhatHaveYouFoundFoundItemForm.text.clear()
+//                binding.etAnyAdditionalInfoFoundItemForm.text.clear()
+//                binding.etILostMyLostForm.text.clear()
+//                binding.etRoomFoundItemForm.text.clear()
+//                binding.etNameFoundItemForm.text.clear()
+//                binding.etPhoneFoundItemForm.text.clear()
+//                binding.etEmailFoundItemForm.text.clear()
+//                sImage = ""
+//
+//                Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+//
+//                val intent = Intent(this@FoundItemFormActivity,MainActivity::class.java )
+//                startActivity(intent)
+//                finish()
+//            }.addOnFailureListener{
+//                Toast.makeText(this,"Failed", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
+//    private val ActivityResultLauncher = registerForActivityResult<Intent, ActivityResult>(
+//        ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+//        if (result.resultCode == RESULT_OK) {
+//            val uri = result.data!!.data
+//            try {
+//                val inputStream = contentResolver.openInputStream(uri!!)
+//                val bitmap = BitmapFactory.decodeStream(inputStream)
+//                val stream = ByteArrayOutputStream()
+//                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+//                val bytes = stream.toByteArray()
+//                sImage = Base64.encodeToString(bytes, Base64.DEFAULT)
+//                binding.ivPhoto.setImageBitmap(bitmap)
+//                inputStream!!.close()
+//
+//            } catch (e: Exception) {
+//                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
     }
 }
