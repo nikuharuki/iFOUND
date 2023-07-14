a5dfe6f260c6c9d5a01e792fde2f325beecbd4a3
Kristel Angela Pastor <202101080@iacademy.edu.ph>
2023-07-14 22:17:46 +0800

    -settings menu, clickable items inside, layout activity_change_username.xml, activity_change_email.xml, activity_change_password.xml

---
+++ app/src/main/java/com/example/ifound/ChangeEmailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ifound
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangeEmailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_email)
+    }
+}
--- app/src/main/java/com/example/ifound/ProfileFragment.kt
+++ app/src/main/java/com/example/ifound/ProfileFragment.kt
@@ -1,59 +1,70 @@
 package com.example.ifound
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.example.ifound.databinding.ActivityChangeEmailBinding
+import com.example.ifound.databinding.ActivityChangePasswordBinding
+import com.example.ifound.databinding.ActivityChangeUsernameBinding
+import com.example.ifound.databinding.FragmentProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentProfileBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        registerForContextMenu(binding.ibtnSettings) // Register the ImageButton for the context menu
+
+        binding.ibtnSettings.setOnClickListener {
+            showPopupMenu(it)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    private fun showPopupMenu(view: View) {
+        val popupMenu = PopupMenu(requireContext(), view)
+        popupMenu.menuInflater.inflate(R.menu.settings_menu, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { item ->
+            handleMenuItemClick(item)
+        }
+        popupMenu.show()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_profile.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun handleMenuItemClick(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.i_username -> {
+                val intent = Intent(requireContext(), ChangeUsernameActivity::class.java)
+                startActivity(intent)
+                true
             }
+            R.id.i_email -> {
+                val intent = Intent(requireContext(), ChangeEmailActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.i_password -> {
+                val intent = Intent(requireContext(), ChangePasswordActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> false
+        }
+    }
+
+
+    override fun onCreateContextMenu(menu: ContextMenu, v: View, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        requireActivity().menuInflater.inflate(R.menu.settings_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return handleMenuItemClick(item)
     }
 }
