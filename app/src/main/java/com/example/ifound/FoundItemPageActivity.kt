4083db9643146220fab29bc3a8521c5c1aa0bb01
Nikos Pasion <nikosladerapasion@gmail.com>
2023-07-14 14:22:02 +0800

    Claim Request, Claim Data. added the delete button and description et.

--- app/src/main/java/com/example/ifound/FoundItemPageActivity.kt
+++ app/src/main/java/com/example/ifound/FoundItemPageActivity.kt
@@ -2,10 +2,21 @@ package com.example.ifound
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ifound.databinding.ActivityFoundItemPageBinding
 
 class FoundItemPageActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityFoundItemPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_found_item_page)
+        binding = ActivityFoundItemPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val foundItem = intent.getParcelableExtra("FoundItemData") as FoundItemData?
+
+        binding.btnAddPhoto.setOnClickListener {
+        ClaimRequestFragment(this, foundItem).show(supportFragmentManager, "ClaimingVerification")
+        }
+
     }
 }
