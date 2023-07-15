17b0e203218ec927a7020ebad816ae58c4b71ce9
Kristel Angela Pastor <202101080@iacademy.edu.ph>
2023-07-15 19:17:39 +0800

    -calendar

--- app/src/main/java/com/example/ifound/ClaimRequestFragment.kt
+++ app/src/main/java/com/example/ifound/ClaimRequestFragment.kt
@@ -1,5 +1,7 @@
 package com.example.ifound
 
+import android.R
+import android.app.DatePickerDialog
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,6 +13,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import java.util.Calendar
 import java.util.UUID
 
 class ClaimRequestFragment(private val context: Context,
@@ -19,6 +22,8 @@ class ClaimRequestFragment(private val context: Context,
 
     private lateinit var binding: FragmentClaimRequestBinding
     private lateinit var database: DatabaseReference
+    private lateinit var datePickerDialog: DatePickerDialog
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -61,10 +66,66 @@ class ClaimRequestFragment(private val context: Context,
                 Toast.makeText(context, "Failed to submit claim request", Toast.LENGTH_SHORT).show()
             }
         }
+
+        binding.btnDatePicker.setOnClickListener {
+            initDatePicker()
+            binding.btnDatePicker.text = getTodaysDate()
+            datePickerDialog.show()
+        }
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:
     Bundle?): View {
         binding = FragmentClaimRequestBinding.inflate(inflater, container, false)
         return binding.root
+    }
+    private fun getTodaysDate(): String {
+        val cal = Calendar.getInstance()
+        val year = cal.get(Calendar.YEAR)
+        val month = cal.get(Calendar.MONTH) + 1
+        val day = cal.get(Calendar.DAY_OF_MONTH)
+        return makeDateString(day, month, year)
+    }
+
+    private fun initDatePicker() {
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                val formattedDate = makeDateString(dayOfMonth, monthOfYear + 1, year)
+                binding.btnDatePicker.text = formattedDate
+            }
+
+        val cal = Calendar.getInstance()
+        val year = cal.get(Calendar.YEAR)
+        val month = cal.get(Calendar.MONTH)
+        val day = cal.get(Calendar.DAY_OF_MONTH)
+
+        val style = R.style.Theme_Holo_Light_Dialog
+
+        datePickerDialog = DatePickerDialog(requireActivity(), style, dateSetListener, year, month, day)
+    }
+
+    private fun makeDateString(day: Int, month: Int, year: Int): String {
+        return getMonthFormat(month) + " " + day + " " + year
+    }
+
+    private fun getMonthFormat(month: Int): String {
+        return when (month) {
+            1 -> "JAN"
+            2 -> "FEB"
+            3 -> "MAR"
+            4 -> "APR"
+            5 -> "MAY"
+            6 -> "JUN"
+            7 -> "JUL"
+            8 -> "AUG"
+            9 -> "SEP"
+            10 -> "OCT"
+            11 -> "NOV"
+            12 -> "DEC"
+            else -> "JAN"
+        }
+    }
+
+    fun openDatePicker(view: View) {
+        datePickerDialog.show()
     }
 }
